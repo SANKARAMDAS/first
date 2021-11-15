aa4ef3bf3eabe9a1ecbf3f484c801f14878cabaa
unnat29 <unnat2904@gmail.com>
2021-11-15 14:02:51 +0530

    Coming soon page frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 
 const App = () => {
+
+  const handleSubmit = (e) => {
+    alert('Response submitted successfully!');
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      Hello World
+    <div className="page">
+      <div className="coming-soon">
+        <div className="content">
+          <h1 className="content__title">
+            Polaris<br />Coming Soon
+          </h1>
+          <p className="content__para">
+            Leave your email with us and we'll drop<br />you an email when its ready.
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input id="email" name="email" type="email" placeholder="Enter your Email Address" />
+            <input type="submit" value="Subscribe" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
