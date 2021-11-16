0666ce49b91490486f983f436cfcbb33fba0470b
unknown <tarang.padia2@gmail.com>
2021-11-16 08:41:23 +0530

    MailChimp Integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,9 @@
 import './App.css';
+import SubscriptionForm from './SubscriptionForm/SubscriptionForm';
 
 const App = () => {
-
-  const handleSubmit = (e) => {
-    alert('Response submitted successfully!');
-    e.preventDefault();
-  }
-
   return (
-    <div className="page">
-      <div className="coming-soon">
-        <div className="content">
-          <h1 className="content__title">
-            Polaris<br />Coming Soon
-          </h1>
-          <p className="content__para">
-            Leave your email with us and we'll drop<br />you an email when its ready.
-          </p>
-          <form onSubmit={handleSubmit}>
-            <input id="email" name="email" type="email" placeholder="Enter your Email Address" />
-            <input type="submit" value="Subscribe" />
-          </form>
-        </div>
-      </div>
-    </div>
+    <SubscriptionForm />
   );
 }
 
--- src/components/SubscriptionForm/SubscriptionForm.js
+++ src/components/SubscriptionForm/SubscriptionForm.js
@@ -1,46 +1,51 @@
 import React, { useState } from "react";
 import axios from "axios";
+import './css/SubscriptionForm.css';
 
 const SubscriptionForm = () => {
 
-    const [formValues, setFormValues] = useState({
-        email: "",
-        FNAME: "",
-        LNAME: "",
-        BIRTHDAY: "",
-        PHONE: "",
-        addr1: "",
-        city: "",
-        country: "",
-        state: "",
-        zip: ""
-    })
+    const [email, setEmail] = useState("")
 
-    let handlerObj;
-
-    const handleFormValuesChange = (selectedInput) => (e) => {
-        handlerObj = { ...formValues }
-        handlerObj[selectedInput] = e.target.value
-        setFormValues({ ...handlerObj })
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
     }
 
-    const handleSubmission = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
-        //Form validation
         axios
-            .post(`${process.env.REACT_APP_BACKEND_API}\subscription\add-member`, formValues)
+            .post(`${process.env.REACT_APP_BACKEND_API}/subscription/add-member`, { email })
             .then((res) => {
                 if (res.data.msg === "SUCCESS") {
-                    console.log("Member Added")
+                    setEmail("")
+                    alert("Your subscription was successful")
                 } else {
-                    console.log("Cannot Add member")
+                    if (res.data.err) {
+                        if (res.data.err.title === "Member Exists") {
+                            alert("You have already subscribed. Please use a different email address.")
+                        }
+                    } else {
+                        alert("Subscription failed due to server error. Please check your network connection and try again.")
+                    }
                 }
             })
     }
 
     return (
-        <div>
-            Hello World
+        <div className="page">
+            <div className="coming-soon">
+                <div className="content">
+                    <h1 className="content__title">
+                        Polaris<br />Coming Soon
+                    </h1>
+                    <p className="content__para">
+                        Leave your email with us and we'll drop<br />you an email when its ready.
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                        <input value={email} onChange={handleEmailChange} id="email" name="email" type="email" placeholder="Enter your Email Address" />
+                        <input type="submit" value="Subscribe" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
