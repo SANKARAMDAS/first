e91d18b78634a739360f6effea16c03f1179d3f8
unknown <tarang.padia2@gmail.com>
2021-11-28 02:17:00 +0530

    Invoice Creation Form Completed

--- src/components/Invoice/CreateInvoice.js
+++ src/components/Invoice/CreateInvoice.js
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import { Form } from 'react-bootstrap';
 import InputRange from 'react-input-range';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import dateFormat from "dateformat";
 import "react-input-range/lib/css/index.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./css/CreateInvoice.css"
 
 const CreateInvoice = () => {
 
+    //State Variables And Handler Variables for CLIENT DETAILS
+
     const [clientValues, setClientValues] = useState({
-        clientName: "",
-        clientEmail: ""
+        name: "",
+        email: ""
     })
 
+    let clientValuesHandler = {}
+
+    //State Variables And Handler Variables for PRODUCT/SERVICE DETAILS
+
+    const [items, setItems] = useState([])
+
+    const [itemDetails, setItemDetails] = useState({
+        name: "",
+        price: "",
+        quantity: 1
+    })
+
+    let itemDetailsHandler = {}
+    let tempItems = []
+
+    //State Variables And Handler Variables for PAYMENT DETAILS
     const [threeSliderValue, setThreeSliderValue] = useState({
         value: { min: 33, max: 66 },
     })
@@ -25,8 +48,54 @@ const CreateInvoice = () => {
         ETH: { selected: false, proportion: 0 }
     })
 
+    const [dueDate, setDueDate] = useState(new Date())
+
     let selectionHandler = {}
 
+    /* CLIENT DETAILS' METHODS */
+
+    //Handler Method
+    const handleClientDetails = (selectedInput) => (e) => {
+        clientValuesHandler = { ...clientValues }
+        clientValuesHandler[selectedInput] = e.target.value
+        setClientValues({ ...clientValuesHandler })
+    }
+
+    /* PRODUCT/ITEM DETAILS' METHODS */
+
+    //Handler Method
+    const handleItemDetails = (selectedInput) => (e) => {
+        itemDetailsHandler = { ...itemDetails }
+        itemDetailsHandler[selectedInput] = e.target.value
+        setItemDetails({ ...itemDetailsHandler })
+    }
+
+    //Add Item
+    const addItem = () => {
+        setItems([...items, itemDetails])
+        clearItemDetails()
+    }
+
+    //Delete Item
+    const deleteItem = (index) => {
+        tempItems = [...items]
+        tempItems.splice(index, 1)
+        setItems([...tempItems])
+    }
+
+    const clearItemDetails = () => {
+        setItemDetails({
+            name: "",
+            price: "",
+            quantity: 1
+        })
+    }
+
+    /* PAYMENT DETAILS' METHODS */
+
+    //Handler Methods
+
+    //Currency Selection Handler
     const handleSelection = (e) => {
         if (e.target.checked === true) {
             setSelected(selected + 1)
@@ -47,44 +116,7 @@ const CreateInvoice = () => {
         }
     }
 
-    const reset = (selectedValue) => {
-        switch (selectedValue) {
-            case 3: {
-                setThreeSliderValue({ value: { min: 34, max: 67 } })
-                setProportionValues({
-                    FIAT: { selected: true, proportion: 34 },
-                    BTC: { selected: true, proportion: 33 },
-                    ETH: { selected: true, proportion: 33 }
-                })
-                break;
-            }
-            case 2: {
-                setTwoSliderValue(50)
-                selectionHandler = { ...proportionValues }
-                for (const select in selectionHandler) {
-                    if (selectionHandler[select].selected === true) {
-                        selectionHandler[select].proportion = 50
-                    } else {
-                        selectionHandler[select].proportion = 0
-                    }
-                }
-                setProportionValues({ ...selectionHandler })
-                break;
-            }
-            default: {
-                selectionHandler = { ...proportionValues }
-                for (const select in selectionHandler) {
-                    if (selectionHandler[select].selected === true) {
-                        selectionHandler[select].proportion = 100
-                    } else {
-                        selectionHandler[select].proportion = 0
-                    }
-                }
-                setProportionValues({ ...selectionHandler })
-            }
-        }
-    }
-
+    //Proportion Slider Handler for 2 SELECTED CURRENCIES
     const handleTwoSlider = (value) => {
         setTwoSliderValue(value)
         let flag = 0
@@ -105,6 +137,24 @@ const CreateInvoice = () => {
 
     }
 
+    //Proportion Slider Handler for 3 SELECTED CURRENCIES
+    const handleThreeSlider = (value) => {
+        setThreeSliderValue({ value })
+        setProportionValues({
+            FIAT: { selected: true, proportion: value['min'] },
+            BTC: { selected: true, proportion: value['max'] - value['min'] },
+            ETH: { selected: true, proportion: 100 - value['max'] }
+        })
+    }
+
+    //Due Date Handler
+
+    const handleDueDateChange = (date) => {
+        setDueDate(date)
+    }
+
+
+    //Proportion Slider Render Methods
     const renderTwoSliderLabels = () => {
         let objArray = []
         selectionHandler = { ...proportionValues }
@@ -144,27 +194,12 @@ const CreateInvoice = () => {
         )
     }
 
-    const handleThreeSlider = (value) => {
-        setThreeSliderValue({ value })
-        setProportionValues({
-            FIAT: { selected: true, proportion: value['min'] },
-            BTC: { selected: true, proportion: value['max'] - value['min'] },
-            ETH: { selected: true, proportion: 100 - value['max'] }
-        })
-    }
-
-    const handlePreviewInvoice = (e) => {
-        e.preventDefault()
-        console.log(selected)
-        console.log(proportionValues)
-    }
-
     const renderSlider = () => {
         switch (selected) {
             case 2:
                 return (
                     <>
-                        <Form.Label style={{ marginTop: "40px" }}>Select Proportion:</Form.Label>
+                        <Form.Label style={{ marginTop: "20px" }}>Select Proportion:</Form.Label>
                         {renderTwoSliderLabels()}
                         <div className="col-12">
                             <InputRange
@@ -181,7 +216,7 @@ const CreateInvoice = () => {
             case 3:
                 return (
                     <>
-                        <Form.Label style={{ marginTop: "40px" }}>Select Proportion:</Form.Label>
+                        <Form.Label style={{ marginTop: "20px" }}>Select Proportion:</Form.Label>
                         {renderThreeSliderLabels()}
                         <div className="col-12">
                             <InputRange
@@ -200,65 +235,138 @@ const CreateInvoice = () => {
         }
     }
 
+    //Proportion Slider Reset Method
+    const reset = (selectedValue) => {
+        switch (selectedValue) {
+            case 3: {
+                setThreeSliderValue({ value: { min: 34, max: 67 } })
+                setProportionValues({
+                    FIAT: { selected: true, proportion: 34 },
+                    BTC: { selected: true, proportion: 33 },
+                    ETH: { selected: true, proportion: 33 }
+                })
+                break;
+            }
+            case 2: {
+                setTwoSliderValue(50)
+                selectionHandler = { ...proportionValues }
+                for (const select in selectionHandler) {
+                    if (selectionHandler[select].selected === true) {
+                        selectionHandler[select].proportion = 50
+                    } else {
+                        selectionHandler[select].proportion = 0
+                    }
+                }
+                setProportionValues({ ...selectionHandler })
+                break;
+            }
+            default: {
+                selectionHandler = { ...proportionValues }
+                for (const select in selectionHandler) {
+                    if (selectionHandler[select].selected === true) {
+                        selectionHandler[select].proportion = 100
+                    } else {
+                        selectionHandler[select].proportion = 0
+                    }
+                }
+                setProportionValues({ ...selectionHandler })
+            }
+        }
+    }
+
+    //Handle Submission To Preview Invoice
+    const handlePreviewInvoice = (e) => {
+        e.preventDefault()
+        console.log(clientValues)
+        console.log(proportionValues)
+        console.log(items)
+        console.log(dateFormat(dueDate, "dd/mm/yyyy"))
+    }
+
     return (
         <>
             <div style={{ marginTop: "70px", marginBottom: "70px" }} className="d-flex justify-content-center align-items-center">
                 <div className="col-lg-6 col-md-6 col-sm-6">
                     <h3 style={{ marginBottom: "20px" }}>Client Details</h3>
                     <Form>
-                        <Form.Group className="mb-3" controlId="formBasicNumber">
+                        <Form.Group onChange={handleClientDetails("name")} value={clientValues['name']} className="mb-3" controlId="formBasicNumber">
                             <Form.Label>Name:</Form.Label>
                             <Form.Control placeholder="Enter Client Name" />
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicNumber">
+                        <Form.Group onChange={handleClientDetails("email")} value={clientValues['email']} className="mb-3" controlId="formBasicNumber">
                             <Form.Label>Email:</Form.Label>
                             <Form.Control placeholder="Enter Client Email" />
                         </Form.Group>
-                        <h3 style={{ marginBottom: "20px" }}>Product Details</h3>
+                        <h3 style={{ marginBottom: "20px" }}>Product/Service Details</h3>
                         <div className="row">
-                            <div className="col-lg-5 col-md-5 col-sm-12">
-                                <Form.Group className="mb-3" controlId="formBasicNumber">
-                                    <Form.Label>Name:</Form.Label>
-                                    <Form.Control placeholder="Enter Product Name" />
-                                </Form.Group>
-                            </div>
-                            <div className="col-lg-5 col-md-5 col-sm-6">
-                                <Form.Group className="mb-3" controlId="formBasicNumber">
-                                    <Form.Label>Price:</Form.Label>
-                                    <Form.Control placeholder="Enter Product Price" />
-                                </Form.Group>
-                            </div>
-                            <div className="col-lg-2 col-md-2 col-sm-6">
-                                <Form.Group className="mb-3" controlId="formBasicNumber">
-                                    <Form.Label>Quantity:</Form.Label>
-                                    <Form.Control type="number" min="1" step="1" />
-                                </Form.Group>
+                            <div className="col-12">
+                                <div className="table table-responsive">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Price</th>
+                                                <th>Quantity</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                items.length === 0 ? <></> :
+                                                    items.map((item, index) => {
+                                                        return (
+                                                            <tr key={index}>
+                                                                <td>{item.name}</td>
+                                                                <td>{item.price}</td>
+                                                                <td>{item.quantity}</td>
+                                                                <td><FontAwesomeIcon onClick={() => { deleteItem(index) }} className="remove-item-btn" icon={faTrash} /></td>
+                                                            </tr>
+                                                        )
+                                                    }
+                                                    )}
+                                            <tr>
+                                                <td>
+                                                    <Form.Control value={itemDetails['name']} onChange={handleItemDetails("name")} placeholder="Enter Name" />
+                                                </td>
+                                                <td>
+                                                    <Form.Control value={itemDetails['price']} onChange={handleItemDetails("price")} placeholder="Enter Price" />
+                                                </td>
+                                                <td>
+                                                    <Form.Control value={itemDetails['quantity']} onChange={handleItemDetails("quantity")} placeholder="Enter Quantity" type="number" min="1" step="1" />
+                                                </td>
+                                                <td><FontAwesomeIcon onClick={addItem} className="add-item-btn" icon={faPlus} /></td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                         <h3 style={{ marginBottom: "20px" }}>Payment Details</h3>
                         <div className="row">
                             <Form.Label>Select Currencies:</Form.Label>
-                            <div className="col-lg-4 col-md-4 col-sm-12">
-                                <Form.Check defaultChecked={true} onChange={handleSelection} type="checkbox" name="FIAT" label="FIAT" />
-                            </div>
-                            <div className="col-lg-4 col-md-4 col-sm-12">
-                                <Form.Check onChange={handleSelection} type="checkbox" name="BTC" label="BTC" />
-                            </div>
-                            <div className="col-lg-4 col-md-4 col-sm-12">
-                                <Form.Check onChange={handleSelection} type="checkbox" name="ETH" label="ETH" />
+                            <div style={{ marginBottom: "20px" }} className="row">
+                                <div className="col-lg-4 col-md-4 col-sm-12">
+                                    <Form.Check defaultChecked={true} onChange={handleSelection} type="checkbox" name="FIAT" label="FIAT" />
+                                </div>
+                                <div className="col-lg-4 col-md-4 col-sm-12">
+                                    <Form.Check onChange={handleSelection} type="checkbox" name="BTC" label="BTC" />
+                                </div>
+                                <div className="col-lg-4 col-md-4 col-sm-12">
+                                    <Form.Check onChange={handleSelection} type="checkbox" name="ETH" label="ETH" />
+                                </div>
                             </div>
                             {renderSlider()}
                             <div className="col-lg-12">
                                 <Form.Group className="mb-3" controlId="formBasicCVV">
                                     <Form.Label>Invoice Due Date:</Form.Label>
-                                    {/* <DatePicker
-                                    selected={expiryDate}
-                                    onChange={(date) => handleExpiryChange(date)}
-                                    onFocus={handleFocus("expiry")}
-                                    placeholderText="MM/YY"
-                                    dateFormat="MM/yy"
-                                    showMonthYearPicker
-                                /> */}
+                                    <DatePicker
+                                        value={dueDate}
+                                        selected={dueDate}
+                                        onChange={(date) => handleDueDateChange(date)}
+                                        placeholderText="DD/MM/YY"
+                                        dateFormat="dd/MM/yyyy"
+                                    />
                                 </Form.Group>
                             </div>
                         </div>
