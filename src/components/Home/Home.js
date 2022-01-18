9ea59a65cd72d8b3b07018aabeb24f89330d2bc0
unnat29 <unnat2904@gmail.com>
2022-01-19 02:36:16 +0530

    Metamask Wallet Connection

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header";
+import Metamask from '../Metamask/Metamask';
 
 const Home = () => {
   return (
     <div className="page">
       <Header />
       <h1>Homepage</h1>
+      <Metamask />
     </div>
   );
 }
