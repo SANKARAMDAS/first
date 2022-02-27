5d6645083bbc50b1c50cdad202956f703ee12d87
unnat29 <unnat2904@gmail.com>
2022-02-27 23:32:10 +0530

    Forgot Password UI

--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import Header from "../Header/Header";
 
 const ForgotPassword = () => {
@@ -13,29 +15,41 @@ const ForgotPassword = () => {
         console.log(res);
         alert('Password Reset Link has been sent successfully to your email!');
       })
+      .catch(err => {
+        console.log(err);
+        alert('This email does not exist!');
+      })
   }
 
   return(
     <div className="page">
       <Header />
-      <div className="auth-page">
-        <div className="box">
-        <h1 className="box__heading">Forgot Password</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              value={email}
-              className="box__input"
-              placeholder="Email Address"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <button className="box__button" type="submit">Send Reset Password Link</button>
-          </form>
-        </div>
-      </div>
+      <Container>
+        <Row className="justify-content-center my-5">
+          <Col lg="4" md="3"></Col>
+          <Col lg="4" md="5" sm="10" xs="10">
+            <div className="box">
+              <h1 className="box__heading">Forgot Password</h1>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="box__form-group" controlId="email">
+                  <Form.Label>Enter Email ID</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={email}
+                    className="box__input"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    placeholder="name@email.com"
+                  />
+                </Form.Group>
+                <button className="box__button" type="submit">Send Reset Password Link</button>
+                <Link className="box__forgot-password" to="/auth">Back to Log in page</Link>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
