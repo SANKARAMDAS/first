a89943929136f1a146bad60a481f3c0388a61c60
unnat29 <unnat2904@gmail.com>
2022-03-30 00:18:27 +0530

    Filter functionality add and Invoice List UI improve

--- src/components/Invoice/DisplayInvoices.js
+++ src/components/Invoice/DisplayInvoices.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import "./css/DisplayInvoices.css"
+import Filter from "./images/Filter.svg";
 
 const DisplayInvoices = (props) => {
 
   const [invoices, setInvoices] = useState([])
+  const [filteredInvoices, setFilteredInvoices] = useState([])
+  const [filterKeyword, setFilteredKeyword] = useState("")
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
@@ -22,7 +25,8 @@ const DisplayInvoices = (props) => {
             }
           )
           .then((response) => {
-            setInvoices(response.data.data)
+            setInvoices(response.data.data.reverse())
+            setFilteredInvoices(response.data.data.reverse())
             console.log(response.data)
           })
           .catch((err) => {
@@ -34,6 +38,26 @@ const DisplayInvoices = (props) => {
   getInvoices()
   }, [])
 
+  const handleFilter = (e) => {
+    e.preventDefault();
+    const f = invoices.filter(invoice => {
+      if(props.role === "freelancer") {
+        return invoice.businessName.toLowerCase().includes(filterKeyword) ||
+        invoice.businessEmail.toLowerCase().includes(filterKeyword) ||
+        invoice.invoiceId.toLowerCase().includes(filterKeyword) ||
+        invoice.status.toLowerCase().includes(filterKeyword) ||
+        invoice.dueDate.toLowerCase().includes(filterKeyword)
+      } else {
+        return invoice.freelancerName.toLowerCase().includes(filterKeyword) ||
+        invoice.freelancerEmail.toLowerCase().includes(filterKeyword) ||
+        invoice.invoiceId.toLowerCase().includes(filterKeyword) ||
+        invoice.status.toLowerCase().includes(filterKeyword) ||
+        invoice.dueDate.toLowerCase().includes(filterKeyword)
+      }
+    })
+    setFilteredInvoices(f)
+  }
+
   const renderInvoices = () => {
     if (isLoading) {
       return (
@@ -43,11 +67,24 @@ const DisplayInvoices = (props) => {
       )
     } else {
       return (
-        <Container className="displayInvoices">
+        <Container fluid className="displayInvoices">
           <h3 className="displayInvoices__heading">Invoices List</h3>
-          <Row>
-            <Col>
-              {invoices.length === 0 ? (
+          <Row className="justify-content-center">
+            <Col lg="11" className="displayInvoices__topSection">
+              <Form.Label>Search by Keyword</Form.Label>
+              <Form onSubmit={handleFilter}>
+                <Form.Control
+                  type="text"
+                  value={filterKeyword}
+                  placeholder="Type search keyword..."
+                  className="input"
+                  onChange={(e) => setFilteredKeyword(e.target.value)}
+                />
+                <button type="submit"><img src={Filter} /> Filter</button>
+              </Form>
+            </Col>
+            <Col lg="11" className="mt-3">
+              {filteredInvoices.length === 0 ? (
                 <>
                   <p>0 records found</p>
                 </>
@@ -56,8 +93,8 @@ const DisplayInvoices = (props) => {
                   <table className="displayInvoices__table mt-2">
                     <thead>
                       <tr>
-                        <th>Invoice No.</th>
-                        <th>{props.role === "freelancer" ? "Business Name" : "Freelancer Name"}</th>
+                        <th>{props.role === "freelancer" ? "Business" : "Freelancer"}</th>
+                        <th>Due Date</th>
                         <th>Total Amount</th>
                         <th>Status</th>
                       </tr>
@@ -66,11 +103,23 @@ const DisplayInvoices = (props) => {
                       {invoices.length === 0 ? (
                           <></>
                       ) : (
-                        invoices.map(invoice => {
+                        filteredInvoices.map(invoice => {
                           return (
                             <tr key={invoice.invoiceId}>
-                              <td><Link className="button" to={`${props.url}/invoices/${invoice.invoiceId}`}>{invoice.invoiceId}</Link></td>
-                              <td>{props.role === "freelancer" ? invoice.businessName : invoice.freelancerName}</td>
+                              <td>
+                                <Link className="button" to={`${props.url}/invoices/${invoice.invoiceId}`}>
+                                  <b>{props.role === "freelancer" ? invoice.businessName : invoice.freelancerName}</b>
+                                  <br />
+                                  <span className="details">
+                                    {props.role === "freelancer" ? invoice.businessEmail : invoice.freelancerEmail}
+                                    <br />
+                                    #{invoice.invoiceId}
+                                    <br />
+                                    Generated at : {invoice.creationDate}
+                                  </span>
+                                </Link>
+                              </td>
+                              <td>{invoice.dueDate}</td>
                               <td>{invoice.totalAmount}</td>
                               <td>
                                 <span className="cancel">{invoice.status === "cancel" ? "Cancel" : ""}</span>
