8cf517fb197637e1adb38cb210d85e594f4f2120
DSankaram <sankaram@octaloop.com>
2022-05-04 16:29:42 +0530

    ui

--- src/components/Contractor/ContractorRoutes.js
+++ src/components/Contractor/ContractorRoutes.js
@@ -1,75 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import isAuthenticated from '../../assets/js/auth';
-import FreelancerDashboard from '../Dashboard/FreelancerDashboard';
-import Profile from '../Profile/Profile';
-import Settings from '../Settings/Settings';
-import InvoiceDetails from '../Invoice/InvoiceDetails';
+import React, { useState, useEffect } from 'react'
+import isAuthenticated from '../../assets/js/auth'
+import FreelancerDashboard from '../Dashboard/FreelancerDashboard'
+import Profile from '../Profile/Profile'
+import Settings from '../Settings/Settings'
+import InvoiceDetails from '../Invoice/InvoiceDetails'
 import DisplayInvoices from '../Invoice/DisplayInvoices'
 import CreateInvoice from '../Invoice/CreateInvoice'
-import Layout from '../Layout/Layout';
-import { Route, useRouteMatch } from 'react-router-dom';
-import WirePayout from '../PayoutMethods/WirePayout';
-import Wireayout from '../PayoutMethods/Wireayout';
+import Layout from '../Layout/Layout'
+import { Route, useRouteMatch } from 'react-router-dom'
+import WirePayout from '../PayoutMethods/WirePayout'
+import Wireayout from '../PayoutMethods/Wireayout'
+import BusinessWalletPayment from '../BusinessPaymentMethod/BusinessWalletPayment'
 
 const ContractorRoutes = () => {
+  let { path, url } = useRouteMatch()
+  const [userAuth, setUserAuth] = useState({})
 
-    let { path, url } = useRouteMatch()
-    const [userAuth, setUserAuth] = useState({})
-
-    useEffect(() => {
-        const checkAuthentication = async () => {
-            setUserAuth(await isAuthenticated())
-        }
-        checkAuthentication()
-    }, []);
-
-    const renderContractorDashboard = () => {
-        if (Object.keys(userAuth) === 0) {
-            return <div><h4>Loading...</h4></div>
-        }
-        else if (userAuth.authenticated) {
-            return (
-                <Layout url={url} role="freelancer">
-                    <Route path={path} exact >
-                        <FreelancerDashboard email={userAuth.email} role={userAuth.role} url={url} />
-                    </Route>
-                    <Route path={`${path}/create-invoice`} exact >
-                        <CreateInvoice email={userAuth.email} />
-                    </Route>
-                    <Route path={`${path}/payout`} exact >
-                        <WirePayout email={userAuth.email} role={userAuth.role} url={url}/>
-                    </Route>
-                    <Route path={`${path}/wireayout`} exact >
-                        <Wireayout email={userAuth.email} role={userAuth.role} url={url}/>
-                    </Route>
-                    <Route path={`${path}/profile`} exact >
-                        <Profile email={userAuth.email} role={userAuth.role} />
-                    </Route>
-                    <Route path={`${path}/settings`} exact >
-                        <Settings email={userAuth.email} />
-                    </Route>
-                    <Route path={`${path}/invoices`} exact >
-                        <DisplayInvoices email={userAuth.email} role={userAuth.role} url={url} />
-                    </Route>
-                    <Route path={`${path}/invoices/:invoiceId`}>
-                        <InvoiceDetails role={userAuth.role} url={url} />
-                    </Route>
-                </Layout>
-            )
-        } else {
-            return (
-                <>
-                    <>Forbidden Error - 403</>
-                </>
-            )
-        }
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      setUserAuth(await isAuthenticated())
     }
+    checkAuthentication()
+  }, [])
 
-    return (
+  const renderContractorDashboard = () => {
+    if (Object.keys(userAuth) === 0) {
+      return (
+        <div>
+          <h4>Loading...</h4>
+        </div>
+      )
+    } else if (userAuth.authenticated) {
+      return (
+        <Layout url={url} role="freelancer">
+          <Route path={path} exact>
+            <FreelancerDashboard
+              email={userAuth.email}
+              role={userAuth.role}
+              url={url}
+            />
+          </Route>
+          <Route path={`${path}/create-invoice`} exact>
+            <CreateInvoice email={userAuth.email} />
+          </Route>
+          <Route path={`${path}/payment`} exact>
+            <BusinessWalletPayment
+              email={userAuth.email}
+              role={userAuth.role}
+              url={url}
+            />
+          </Route>
+          <Route path={`${path}/payout`} exact>
+            <WirePayout email={userAuth.email} role={userAuth.role} url={url} />
+          </Route>
+          <Route path={`${path}/wireayout`} exact>
+            <Wireayout email={userAuth.email} role={userAuth.role} url={url} />
+          </Route>
+          <Route path={`${path}/profile`} exact>
+            <Profile email={userAuth.email} role={userAuth.role} />
+          </Route>
+          <Route path={`${path}/settings`} exact>
+            <Settings email={userAuth.email} />
+          </Route>
+          <Route path={`${path}/invoices`} exact>
+            <DisplayInvoices
+              email={userAuth.email}
+              role={userAuth.role}
+              url={url}
+            />
+          </Route>
+          <Route path={`${path}/invoices/:invoiceId`}>
+            <InvoiceDetails role={userAuth.role} url={url} />
+          </Route>
+        </Layout>
+      )
+    } else {
+      return (
         <>
-            {renderContractorDashboard()}
+          <>Forbidden Error - 403</>
         </>
-    );
+      )
+    }
+  }
+
+  return <>{renderContractorDashboard()}</>
 }
 
-export default ContractorRoutes;
+export default ContractorRoutes
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,30 +1,32 @@
-import React from "react";
-import axios from "axios";
-import { Link, useHistory } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import "./Layout.css";
-import dashboard from "./Icons/dashboard.svg";
-import invoice from "./Icons/invoice.svg";
-import profile from "./Icons/profile.svg";
-import settings from "./Icons/settings.svg";
-import logout from "./Icons/logout.svg";
-import question from "./Icons/question-mark.svg";
-import payoutmethods from "./Icons/Payment-method-outline.svg";
+import React from 'react'
+import axios from 'axios'
+import { Link, useHistory } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import './Layout.css'
+import dashboard from './Icons/dashboard.svg'
+import invoice from './Icons/invoice.svg'
+import profile from './Icons/profile.svg'
+import settings from './Icons/settings.svg'
+import logout from './Icons/logout.svg'
+import question from './Icons/question-mark.svg'
+import payoutmethods from './Icons/Payment-method-outline.svg'
 
 const Layout = (props) => {
-
   const history = useHistory()
 
   const handleLogout = async () => {
     axios.defaults.withCredentials = true
-    await axios.post(`${process.env.REACT_APP_BACKEND_API}/auth/logout`, {
-        withCredentials: true
-    }).then((res) => {
+    await axios
+      .post(`${process.env.REACT_APP_BACKEND_API}/auth/logout`, {
+        withCredentials: true,
+      })
+      .then((res) => {
         console.log(res.data)
-        history.push("/")
-    }).catch((err) => {
+        history.push('/')
+      })
+      .catch((err) => {
         console.log(err)
-    })
+      })
   }
 
   return (
@@ -39,11 +41,17 @@ const Layout = (props) => {
             <img src={invoice} alt="invoice" />
             Invoices
           </Link>
-          {
-            props.role === "freelancer"
-              ? <Link className="button" to={`${props.url}/payout`}><img src={payoutmethods} alt="payout-method" />Payout Methods</Link>
-              : <></>
-          }
+          {props.role === 'freelancer' ? (
+            <Link className="button" to={`${props.url}/payout`}>
+              <img src={payoutmethods} alt="payout-method" />
+              Payout Methods
+            </Link>
+          ) : (
+            <Link className="button" to={`${props.url}/payment`}>
+              <img src={payoutmethods} alt="payout-method" />
+              Payment Methods
+            </Link>
+          )}
           <Link className="button" to={`${props.url}/profile`}>
             <img src={profile} alt="profile" />
             Profile
@@ -57,9 +65,20 @@ const Layout = (props) => {
             Logout
           </button>
         </div>
-        <main className="main" style={{ width: "100%", backgroundColor: "#F5F5F5" }}>
+        <main
+          className="main"
+          style={{ width: '100%', backgroundColor: '#F5F5F5' }}
+        >
           <Container fluid className="betaBar">
-            This is a beta version of Binamite. If you have some feedback let us know here. <span className="tool-tip"><img src={question} /> <span className="tool-tip-text">Binamite is currently in beta version. In beta version payment and payout methods are inactive.</span></span>
+            This is a beta version of Binamite. If you have some feedback let us
+            know here.{' '}
+            <span className="tool-tip">
+              <img src={question} />{' '}
+              <span className="tool-tip-text">
+                Binamite is currently in beta version. In beta version payment
+                and payout methods are inactive.
+              </span>
+            </span>
           </Container>
           <Container fluid className="py-4">
             {props.children}
@@ -70,4 +89,4 @@ const Layout = (props) => {
   )
 }
 
-export default Layout;
+export default Layout
